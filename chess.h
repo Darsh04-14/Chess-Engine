2f34c57afd43bdb7f8e74c67c3e5e629e1e0e7ba
Darsh04-14 <darshpatelcr7@gmail.com>
2024-06-08 13:03:19 -0400

    Piece struct

---
+++ chess.h
@@ -0,0 +1,16 @@
+
+struct Peice {
+    const int King = 75;
+    const int Queen = 81;
+    const int Knight = 78;
+    const int Bishop = 88;
+    const int Rook = 82;
+    const int Pawn = 80;
+
+    const int White = (1 << 7);
+    const int Black = (1 << 8);
+};
+
+class Chess {
+
+};
